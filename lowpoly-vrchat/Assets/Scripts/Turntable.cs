cfdefd55bdbf51bd9f1d7f0e934867c153c7f973
Nagitch <mujinaproduction@gmail.com>
2021-02-13 21:31:20 +0900

    setup studio preview

---
+++ lowpoly-vrchat/Assets/Scripts/Turntable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turntable : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.1f;
+    
+    void Update()
+    {
+        target.Rotate(new Vector3(.0f,this.speed,.0f));
+    }
+}
